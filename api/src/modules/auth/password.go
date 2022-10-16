fb4be871b98db788c42a69a2818ef022f6005b87
muraikenta <muraikenta0507@gmail.com>
2022-10-16 22:28:06 +0900

    tweak

--- api/src/modules/auth/password.go
+++ api/src/modules/auth/password.go
@@ -2,10 +2,6 @@ package auth
 
 import "golang.org/x/crypto/bcrypt"
 
-func EncryptPassword(s string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(s), 12)
-	if err != nil {
-		return "", err
-	}
-	return string(hash), nil
+func EncryptPassword(s string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(s), 12)
 }
