e449f48976a52a1f94b8a24a56ac0702f9dae463
muraikenta <muraikenta0507@gmail.com>
2022-10-29 18:04:57 +0900

    Implement PostService.find

--- api/src/modules/post/post_service.go
+++ api/src/modules/post/post_service.go
@@ -4,13 +4,34 @@ import (
 	"context"
 	"simple_sns_api/src/db"
 	"simple_sns_api/src/ent"
+	"simple_sns_api/src/ent/post"
 )
 
 type PostService struct{}
 
+type PaginationParams struct {
+	Cursor int
+	Size   int
+}
+
 type CreateParams struct {
 	UserId int
 	Body   string
+}
+
+func (s PostService) find(ctx context.Context, pagination PaginationParams) ([]*ent.Post, error) {
+	query := db.Client.Post.Query()
+	if pagination.Cursor != 0 {
+		query = query.Where(post.IDLT(pagination.Cursor))
+	}
+	if pagination.Size == 0 {
+		pagination.Size = 20
+	}
+	posts, err := query.
+		Limit(pagination.Size).
+		Order(ent.Desc(post.FieldID)).
+		All(ctx)
+	return posts, err
 }
 
 func (s PostService) Create(ctx context.Context, params CreateParams) (*ent.Post, error) {
